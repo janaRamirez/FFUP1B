ec24c5cc6ff0482c541d839caac5513627811db5
janaRamirez <lizz5259@hotmail.com>
2015-11-26 23:17:48 -0600

    JANA

---
+++ tarea_char_2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>//
+char cadena[50];
+char cadena2[50];
+int i=0;
+int j=0;
+int main ()
+{
+	scanf("%s",cadena);
+	scanf("%s",cadena2);
+	while(cadena[i]==cadena2[j])
+	{
+		while(cadena[i]==cadena2[j])
+		{
+			i++;
+			j++;
+			if(cadena2[j]=='\0')
+			printf("existe\n");
+		break;
+		}
+		}
+		i++;
+		j=0;
+	  for ( i=0; i=!j ;i++)
+		{
+			if(cadena[i]==cadena2[j])
+			i++;
+			j++;
+		printf(" no existe\n");
+		
+		}
+		    
+	}
+		
+
+		
+		
+	
+
